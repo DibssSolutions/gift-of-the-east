7813ee9c0f0519b8599def8c8aa5a06c27a88b6e
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-10-11 10:18:09 +0300

    modal-feedback adoptive

--- src/js/components/_file.js
+++ src/js/components/_file.js
@@ -4,7 +4,7 @@ const preview = document.querySelector('.js-file-preview');
 function showPreview() {
   let uploaded = file.files[0];
   uploaded.name
-    ? (preview.innerHTML = `Вы загрузили файл <strong>${
+    ? (preview.innerHTML = `Вы загрузили файл<strong>${
       uploaded.name
     }</strong>`)
     : (preview.innerHTML = '');
