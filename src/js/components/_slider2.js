e74c6b9eb46d343605d18821ec0dfba4a3552eb2
Roman Muratov <myratov.r@gmail.com>
2018-12-14 12:11:20 +0200

    changed you watched slider number of displayed elements

--- src/js/components/_slider2.js
+++ src/js/components/_slider2.js
@@ -249,7 +249,7 @@ sliderWatched.slick({
   dots: false,
   infinite: false,
   speed: 300,
-  slidesToShow: 4,
+  slidesToShow: 3,
   slidesToScroll: 1,
   prevArrow: `<button class="slider-watched__prev" type="button">${buildIcon(
     'arrow-left'
